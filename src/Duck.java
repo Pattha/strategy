693ca3a67646149e70b5137e34e79c78b2eeeb3d
Pattha <30823977+Pattha@users.noreply.github.com>
2017-08-08 16:33:56 +0700

    Initial commit

---
+++ src/Duck.java
@@ -0,0 +1,12 @@
+public abstract class Duck {
+   void quack(){
+	System.out.println("Quack! Quack!");
+   }
+   void swim(){
+	   System.out.println("Swiming");
+   }
+   abstract void display();
+   void fly(){
+	   System.out.println("I'm flying");
+}
+}
---
+++ src/TestDriver.java
@@ -0,0 +1,21 @@
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		MallarDuck d1 = new MallarDuck();
+		d1.quack();
+		d1.display();
+		RubberDuck d2 = new RubberDuck();
+		d2.quack();
+		d2.fly();
+		d2.display();
+		Duck d3 = new RedheadDuck();
+		d3.quack();
+		d3.display();
+		d3 = new RubberDuck();
+		d3.quack();
+		d3.fly();
+		d3.display();
+	}
+
+}
